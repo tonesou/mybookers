af4e0755d102f9a2f1fb7f14c918b434a1f50468
tonesou <tonegawa0525@gmail.com>
2022-08-14 14:06:54 +0000

    create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,16 +3,25 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render index
+    end
+  end
+
   def show
   end
 
   def edit
   end
-  
+
   private
-  
+
   def book_params #ストロングパラメータ
     params.require(:book).permit(:title, :body) #パラメーターのキー
   end
-  
+
 end
