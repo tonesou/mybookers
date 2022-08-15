71cd20fdaee110574ff07a7a359c918691649917
tonesou <tonegawa0525@gmail.com>
2022-08-15 11:13:30 +0000

    destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,6 +29,12 @@ class BooksController < ApplicationController
       render "edit"
     end
   end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
 
   private
 
