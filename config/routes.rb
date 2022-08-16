fd7c3e6ffb5f9d59700a0ded035dbbf6bcc02ff3
tonesou <tonegawa0525@gmail.com>
2022-08-16 14:15:56 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
   resources :books
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
