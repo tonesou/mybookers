5db544b498d7d8e4208e6683dd83dec0ded3578e
tonesou <tonegawa0525@gmail.com>
2022-08-19 06:44:50 +0000

    user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = Book.new
-    @books = @user,books
+    @books = @user.books
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2022_08_13_153518) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
